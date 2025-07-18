6dd408b3ba11406ebe41fa1844be3fcdfe8d8273
Judah Kahler <judah.kahler@gmail.com>
2025-07-18 17:47:04 -0500

    First PHP program

---
+++ firstProgram/index.php
@@ -0,0 +1,26 @@
+<?php
+    // This is how you write a note.
+    /*
+        Multi-
+        -line
+        comment
+    */
+
+    echo"Hello World! <br>";
+    echo"BLAME! is great!";
+
+    // PHP files can have HTML, CSS, and JavaScript!!!
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>Order a Pizza</button>
+</body>
+</html>
