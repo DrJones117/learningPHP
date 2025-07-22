dcb6f7edc7e0375936b342091254808e671e9411
Judah Kahler <judah.kahler@gmail.com>
2025-07-22 14:06:44 -0500

    Arithmetic

---
+++ 3arimetic/index.php
@@ -0,0 +1,33 @@
+<?php
+    // arithmetic operators
+    // + - * / ** %
+
+    $x = 10;
+    $y = 2;
+    $z = null;
+
+    // $z = $x + $y;
+    // $z = $x - $y;
+    // $z = $x * $y;
+    // $z = $x / $y;
+    // $z = $x / $y;
+    // $z = $x ** $y;
+    $z = $x % $y;
+    echo $z;
+
+    echo "<br>";
+
+    $counter = 0;
+
+    $counter++;
+
+    echo $counter;
+
+    // You already know all of the increment and decrement operator.
+
+    // Operator Precedence
+    // ()
+    // **
+    // * / %
+    // + -
+?>
