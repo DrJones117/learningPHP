05a0c4907f2e1070a093ab00287879772c9e0b4f
Judah Kahler <judah.kahler@gmail.com>
2025-08-14 14:13:32 -0500

    include function

---
+++ 19include/index.php
@@ -0,0 +1,21 @@
+<?php
+    // copies the content of a file and includes it in your php file.
+    // Reusable sections of a website
+
+    include("header.html");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    This is the home page<br>
+    Stuff about the home may go here.
+</body>
+</html>
+<?php
+    include("footer.html");
+?>
