e4477aeb281206a343f7e37c68194d665000fab9
Judah Kahler <judah.kahler@gmail.com>
2025-10-01 19:00:08 -0500

    Inroduction to $_SERVER

--- 22$_SERVER/index.php
+++ 22$_SERVER/index.php
@@ -1,8 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post">
+        username:<br>
+        <input type="text" name="username">
+        <input type="submit">
+    </form>
+</body>
+</html>
 <?php
     // Shows everything you need to know about the current web page enviroment.
 
 
-    foreach ($_SERVER as $key => $value) {
-        echo "{$key} = {} <br>";
+    // foreach ($_SERVER as $key => $value) {
+    //     echo "{$key} = {} <br>";
+    // }
+
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        echo "hello";
     }
 ?>
