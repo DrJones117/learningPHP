7d0aa343903d0fee094268c195a1d3cf2a0cea2d
Judah Kahler <judah.kahler@gmail.com>
2025-10-29 19:05:05 -0500

    Inset data via php

---
+++ 26insertintodatabase/database.php
@@ -0,0 +1,17 @@
+<?php
+    $db_server = "localhost";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "businessdb";
+
+    try {
+        $connection = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+    }
+    catch (mysqli_sql_exception) {
+        echo "Could not connect <br>";
+    }
+
+    if ($connection) {
+        echo "You are connected! <br>";
+    }
+?>
---
+++ 26insertintodatabase/index.php
@@ -0,0 +1,20 @@
+<?php
+    include("database.php");
+
+    $username = "Spike";
+    $password = "Julia";
+    $hash = password_hash($password, PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users (user, password)
+            VALUES ('$username', '$hash')";
+
+    try{
+        mysqli_query($connection, $sql);
+        echo "User is now registered";
+    }
+    catch (mysqli_sql_exception) {
+        echo "Could not register user";
+    }
+
+    mysqli_close($connection);
+?>
